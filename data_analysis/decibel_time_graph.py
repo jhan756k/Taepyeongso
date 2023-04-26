43b52a657633523ae1bcaaf8455f9f5d0991c9b0
Jooney <jhan756k@gmail.com>
2023-04-26 15:26:40 +0900

    decibel

---
+++ data_analysis/decibel_time_graph.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import wave
+
+# Open the audio file and extract its properties
+audio_file = wave.open("tp.wav", "rb")
+sample_rate = audio_file.getframerate()
+num_frames = audio_file.getnframes()
+duration = num_frames / float(sample_rate)
+time_limit = duration
+
+# Read the audio data into a numpy array
+audio_data = np.frombuffer(audio_file.readframes(num_frames), dtype=np.int16)
+
+# Convert the amplitude to decibels
+amplitude_dB = 20 * np.log10(np.abs(audio_data))
+
+# Calculate the time values corresponding to each sample
+time_values = np.linspace(0, time_limit, int(sample_rate * time_limit))
+
+# Only include the corresponding values of amplitude_dB up to 0.1 seconds
+amplitude_dB = amplitude_dB[:int(sample_rate * time_limit)]
+
+# Plot the audio signal as a function of time in decibels
+plt.plot(time_values, amplitude_dB)
+plt.xlabel('Time (seconds)')
+plt.ylabel('Amplitude (dB)')
+plt.show()
