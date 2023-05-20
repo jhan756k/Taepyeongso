c60d07d62ecde6aafe786e78c6b52ffe8459075c
Jooney <jhan756k@gmail.com>
2023-05-21 01:48:18 +0900

    graph

--- arduino/sketch_may16b/sketch_may16b.ino
+++ arduino/sketch_may16b/sketch_may16b.ino
@@ -2,7 +2,8 @@
 #include <Adafruit_BMP085.h>
 
 Adafruit_BMP085 bmp;
-int32_t prev = 0, cur;
+int32_t prev = 0;
+double pas, atm;
 double DELTA_T = 0.1, time = 0.0;
 
 void setup() {
@@ -13,12 +14,13 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("Pressure = ");
-  cur = bmp.readPressure();
-  Serial.println(String(cur) + " Pa");
-  Serial.print("Difference : " + String(cur - prev));
+  pas = bmp.readPressure();
+  atm = pas/101325.0;
+  Serial.print(String(pas) + " Pa, ");
+  Serial.println(String(atm) + " atm");
+  Serial.print("Difference : " + String(pas - prev));
   Serial.println(", Time : " + String(time));
-  prev = cur;
+  prev = pas;
   time += DELTA_T;
 
   Serial.println();
--- data_analysis/decibel_time_graph.py
+++ data_analysis/decibel_time_graph.py
@@ -3,11 +3,18 @@ import numpy as np
 import wave
 
 # Open the audio file and extract its properties
-audio_file = wave.open(r"C:\Users\Jooney Han\Desktop\과학전람회\data\sound\15-길이-임계.wav", "rb")
+audio_file = wave.open(r"C:\Users\Jooney Han\Desktop\과학전람회\충음실 데이터\자름_음원\20안자름.wav", "rb")
 sample_rate = audio_file.getframerate()
 num_frames = audio_file.getnframes()
 duration = num_frames / float(sample_rate)
-time_limit = 13
+
+# Define the start time and end time for plotting
+start_time = 1.5  # Modify the start time as desired
+end_time = 4
+
+# Calculate the corresponding start and end frame indices
+start_frame = int(start_time * sample_rate)
+end_frame = int(end_time * sample_rate)
 
 # Read the audio data into a numpy array
 audio_data = np.frombuffer(audio_file.readframes(num_frames), dtype=np.int16)
@@ -16,13 +23,19 @@ audio_data = np.frombuffer(audio_file.readframes(num_frames), dtype=np.int16)
 amplitude_dB = 20 * np.log10(np.abs(audio_data))
 
 # Calculate the time values corresponding to each sample
-time_values = np.linspace(0, time_limit, int(sample_rate * time_limit))
+time_values = np.linspace(0, duration, num_frames)
 
-# Only include the corresponding values of amplitude_dB up to 0.1 seconds
-amplitude_dB = amplitude_dB[:int(sample_rate * time_limit)]
+# Only include the corresponding values of amplitude_dB within the desired time range
+amplitude_dB = amplitude_dB[start_frame:end_frame]
+time_values = time_values[start_frame:end_frame]
 
 # Plot the audio signal as a function of time in decibels
 plt.plot(time_values, amplitude_dB)
 plt.xlabel('Time (seconds)')
 plt.ylabel('Amplitude (dB)')
+
+# Manipulate x-axis ticks
+plt.xticks(np.linspace(start_time, end_time, num=32))
+plt.gca().xaxis.set_major_locator(plt.MaxNLocator(32))
+
 plt.show()
